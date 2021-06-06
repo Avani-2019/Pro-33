19376e8d2e3d335b22bb4493ab0250fa085a2faf
Avani-2019 <72139667+Avani-2019@users.noreply.github.com>
2021-06-06 12:34:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,103 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight=300;
+var score =0;
+var particle;
+var count = 0;
+var gameState = "play";
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+
+   for (var i = 0; i <=width; i = i + 80) {
+     divisions.push(new Divisions(i, 676, 10, 210));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ text("Score : "+score,20,30);
+ text(500 ,25,600);
+ text(500 ,105,600);
+ text(500 ,185,600);
+ text(500 ,265,600);
+ text(100 ,345,600);
+ text(100 ,425,600);
+ text(100 ,505,600);
+ text(200 ,585,600);
+ text(200 ,665,600);
+ text(200 ,745,600);
+  Engine.update(engine);
+ 
+  
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+   if(frameCount%60===0){
+     particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+     score++;
+   }
+ 
+  for (var j = 0; j < particles.length; j++) {
+   
+     particles[j].display();
+   }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+  
+  
+}
+
+function mousePressed() //additonal goal 1
+{
+  if(gameState !== "end"){
+    particle = new Particle(mouseX,10,10,10)
+  }
+}
